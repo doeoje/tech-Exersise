030d75b23fdb83e342849b4fcb4ce75318456a51
doeoje <13doejoe@gmail.com>
2020-10-17 19:51:01 -0500

    Updated Util to process more requests

--- TechExercise/src/util/UtilDB.java
+++ TechExercise/src/util/UtilDB.java
@@ -12,7 +12,6 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
-
 import datamodel.Item;
 
 import org.hibernate.HibernateException;
@@ -20,61 +19,112 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-
-
 /**
  * @since JavaSE-1.8
  */
 public class UtilDB {
-   static SessionFactory sessionFactory = null;
+	static SessionFactory sessionFactory = null;
 
-   public static SessionFactory getSessionFactory() {
-      if (sessionFactory != null) {
-         return sessionFactory;
-      }
-      Configuration configuration = new Configuration().configure();
-      StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-      sessionFactory = configuration.buildSessionFactory(builder.build());
-      return sessionFactory;
-   }
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory != null) {
+			return sessionFactory;
+		}
+		Configuration configuration = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties());
+		sessionFactory = configuration.buildSessionFactory(builder.build());
+		return sessionFactory;
+	}
 
-   public static List<Item> listItem() {
-      List<Item> resultList = new ArrayList<Item>();
+	public static List<Item> listItem() {
+		List<Item> resultList = new ArrayList<Item>();
 
-      Session session = getSessionFactory().openSession();
-      Transaction tx = null;  // each process needs transaction and commit the changes in DB.
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null; // each process needs transaction and commit the changes in DB.
 
-      try {
-         tx = session.beginTransaction();
-         List<?> Items = session.createQuery("FROM Item").list();
-         for (Iterator<?> iterator = Items.iterator(); iterator.hasNext();) {
-            Item Item = (Item) iterator.next();
-            resultList.add(Item);
-         }
-         tx.commit();
-      } catch (HibernateException e) {
-         if (tx != null)
-            tx.rollback();
-         e.printStackTrace();
-      } finally {
-         session.close();
-      }
-      return resultList;
-   }
+		try {
+			tx = session.beginTransaction();
+			List<?> Items = session.createQuery("FROM Item").list();
+			for (Iterator<?> iterator = Items.iterator(); iterator.hasNext();) {
+				Item Item = (Item) iterator.next();
+				resultList.add(Item);
+			}
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return resultList;
+	}
 
-   public static void createItems(String itemname, Double cost, Date date) {
-      Session session = getSessionFactory().openSession();
-      Transaction tx = null;
-      try {
-         tx = session.beginTransaction();
-         session.save(new Item(itemname,cost,date));
-         tx.commit();
-      } catch (HibernateException e) {
-         if (tx != null)
-            tx.rollback();
-         e.printStackTrace();
-      } finally {
-         session.close();
-      }
-   }
+	public static List<Item> listItem(int month) {
+		List<Item> resultList = new ArrayList<Item>();
+
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null; // each process needs transaction and commit the changes in DB.
+
+		try {
+			tx = session.beginTransaction();
+			List<?> Items = session.createQuery("FROM Item").list();
+			for (Iterator<?> iterator = Items.iterator(); iterator.hasNext();) {
+				Item Item = (Item) iterator.next();
+				if (Item.thisMonth(month))
+					resultList.add(Item);
+			}
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return resultList;
+	}
+
+	public static void createItems(String itemname, Double cost, Date date) {
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			session.save(new Item(itemname, cost, date));
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
+
+	public static void togglePaid(int i) {
+		List<Item> resultList = new ArrayList<Item>();
+
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null; // each process needs transaction and commit the changes in DB.
+
+		try {
+			tx = session.beginTransaction();
+			String query = "FROM Item WHERE id = " + i;
+			System.out.println(query);
+			List<?> Items = session.createQuery(query).list();
+			for (Iterator<?> iterator = Items.iterator(); iterator.hasNext();) {
+				Item Item = (Item) iterator.next();
+				Item.setPaid(!Item.getPaid());
+			}
+			tx.commit();
+
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+
+		}
+	}
 }
